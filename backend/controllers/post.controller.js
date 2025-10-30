26a85e5a6b0b0c9b743fe7c25d7c676595c89426
Shubham Zawar <shubhamzawar55@gmail.com>
2025-10-30 15:58:02 +0530

    Worked on creating post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,7 +2,30 @@ import User from "../models/user.model.js";
 import Profile from "../models/profile.model.js";
 
 
+
 export const activeCheck = async(req,res)=>{
     return res.status(200).json({message:"Running"});
 }
 
+export const createPost = async(req,res)=>
+{
+    const {token} = req.body;
+    try
+    {
+        const user = await User.findOne({token});
+        if(!user)
+        {
+            return res.status(404).json({message:"user not found"});
+        }
+        const post = new Post({
+            userId:user._id,
+            body : req.body.body,
+            media:req.file!=undefined?req.file.filename:"",
+            fileType:req.file!=undefined?req.file.mimetype.split("/")[1]:""
+        })
+        await post.save();
+        return res.status(200).json({message:"Post created"});
+    }catch(error){
+        return res.status(500).json({message:error.message});
+    }
+}
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,8 +1,20 @@
 import {Router} from "express";
-import { activeCheck } from "../controllers/post.controller.js";
-
+import { activeCheck, createPost } from "../controllers/post.controller.js";
+import multer from "multer";
 const router = Router();
 
-router.route('/').get(activeCheck);
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'uploads/')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+const upload = multer({storage:storage});
+
+router.route('/').get(activeCheck);
+router.route("/post").post(upload.single('media'),createPost)
 export default router;
