15fd694b18e4088d3ad09635d0e2d6f902d187a6
Shubham Zawar <shubhamzawar55@gmail.com>
2025-11-03 17:24:01 +0530

    worked on  create new comment

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -292,3 +292,39 @@ export const acceptRequest = async(req,res)=>{
         return res.status(500).json({message:error.message});
     }
 }
+
+
+export const commentPost = async(req,res)=>
+{
+    const {token,post_id,commentBody} = req.body;
+    try
+    {
+        const user = await User.findOne({token:token}).select("_id");
+        if(!user)
+        {
+            return res.status(404).json({message:"User not found"});
+        }
+        const post = await Post.findOne({
+            _id:post_id
+        });
+        if(!post)
+        {
+            return res.status(404).json({message:"Post not found"});
+        }
+        const comment = new Comment ({
+            userId:user._id,
+            postId:post_id,
+            comment:commentBody
+        });
+
+        await comment.save();
+
+        return res.status(200).json({message:"Comment added"});
+
+
+    }
+    catch(error)
+    {
+        return res.status(500).json({message:error.message});
+    }
+}
