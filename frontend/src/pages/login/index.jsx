8a70f9b7926dbc143ac633d2e9ed3fa0fa49c517
Shubham Zawar <shubhamzawar55@gmail.com>
2025-11-10 15:29:00 +0530

    worked on registration of user

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,13 +1,19 @@
 import UserLayout from '@/layout/UserLayout';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './style.module.css';
+import { loginUser, registerUser } from '@/config/redux/action/authAction';
 
 function LoginComponent() {
   const authState = useSelector((state) => state.auth);
   const router = useRouter();
+  const dispath = useDispatch();
   const [isLoginMode, setIsLoginMode] = useState(false);
+  const [email,setEmailAddress] = useState("");
+  const [password,setPassword] = useState("");
+  const [username,setUsername] = useState("");
+  const [name,setName] = useState("");
 
   useEffect(() => {
     if (authState.loggedIn) {
@@ -15,22 +21,35 @@ function LoginComponent() {
     }
   }, [authState.loggedIn]);
 
+  const handleRegister = () =>{
+    console.log("REGISTERING...")
+    dispath(registerUser({username,password,email,name}))
+  }
   return (
     <UserLayout>
       <div className={styles.container}>
       <div className={styles.cardContainer}>
         <div className={styles.cardContainer_left}>
           <p className={styles.cardleft_heading}>{isLoginMode ? 'Sign In' : 'Sign Up'}</p>
+          <p style={{color:authState.isError ? "red":"green"}}>{authState.message.message}</p>
 
           <div className={styles.inputContainer}>
             <div className={styles.inputRow}>
-              <input className={styles.inputField} type='text' placeholder='username'/>
-              <input className={styles.inputField} type='text' placeholder='name'/>
+              <input onChange={(e)=>setUsername(e.target.value)} className={styles.inputField} type='text' placeholder='username'/>
+              <input  onChange={(e)=>setName(e.target.value)} className={styles.inputField} type='text' placeholder='name'/>
             </div>
-            <input className={styles.inputField} type='text' placeholder='email'/>
-            <input className={styles.inputField} type='text' placeholder='password'/>
+            <input onChange={(e)=>setEmailAddress(e.target.value)} className={styles.inputField} type='text' placeholder='email'/>
+            <input onChange={(e)=>setPassword(e.target.value)} className={styles.inputField} type='text' placeholder='password'/>
 
-            <div className={styles.buttonWithOutline}>
+            <div onClick={()=>{
+              if(isLoginMode)
+              {
+                handleLogin();
+
+              }else{
+                handleRegister();
+              }
+            }}className={styles.buttonWithOutline}>
               <p>{isLoginMode ? 'Sign In' : 'Sign Up'}</p>
             </div>
             
